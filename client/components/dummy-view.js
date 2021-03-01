692361fb4f246c45177670dc2b5b4115d14373ae
Игорь <igorkhvorost@gmail.com>
2021-03-01 17:25:48 +0300

    1march_work

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -1,16 +1,22 @@
 import React from 'react'
-
+import { Switch, Route } from 'react-router-dom'
 import Head from './head'
+import Header from './header'
+
+import Main from './main'
+import RepoList from './repolist'
+import Text from './text'
 
 const Dummy = () => {
   return (
     <div>
       <Head title="Hello" />
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          This is dummy component
-        </div>
-      </div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={() => <Main />} />
+        <Route exact path="/:userName/" component={() => <RepoList />} />
+        <Route exact path="/:UserName/:repositoryName" component={() => <Text />} />
+      </Switch>
     </div>
   )
 }
