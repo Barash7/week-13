ede2625f567c3f836168ddc77a2eb776a943b2f9
Игорь <igorkhvorost@gmail.com>
2021-03-01 17:35:49 +0300

    1marchSuccess

---
+++ client/components/repolist.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Repolist = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          {/* add your routes here */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Repolist.propTypes = {}
+
+export default Repolist
---
+++ client/components/text.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Text = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          {/* add your routes here */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Text.propTypes = {}
+
+export default Text
