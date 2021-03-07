409aad8d4e2c6729421f056c23ee965d5f09be3a
Игорь <igorkhvorost@gmail.com>
2021-03-07 16:36:29 +0300

    still working

--- client/components/header.js
+++ client/components/header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        Header
+        LOVE
       </div>
     </nav>
   )
--- client/components/main.js
+++ client/components/main.js
@@ -14,7 +14,7 @@ const Main = () => {
     <div className="flex items-center justify-center h-screen">
       <div className="bg-indigo-800 text-black font-bold rounded-lg border shadow-lg p-10">
         <input id="input-field" type="text" value={value} onChange={onChange} /> 
-        <button id="search-button" type="button" onClick={onClick}>
+        <button className="text-white m-4" id="search-button" type="button" onClick={onClick}>
             Send
         </button>
       </div>
